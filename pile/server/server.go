5ce075f9191104cd86decd2d286af784b7a29dd4
Maciek Borzecki <maciek.borzecki@gmail.com>
2022-04-10 19:47:34 +0200

    pile/server: grpc server wrapper
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ pile/server/server.go
@@ -0,0 +1,198 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sort"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/bboozzoo/piled/pile/auth"
+	pb "github.com/bboozzoo/piled/pile/proto"
+	"github.com/bboozzoo/piled/runner"
+)
+
+type piled struct {
+	pb.UnimplementedJobPileManagerServer
+	runner Runner
+
+	// dummy authorization which maps a token to a list of operations
+	opAuthz map[string][]string
+}
+
+type Runner interface {
+	Start(config runner.Config) (name string, err error)
+	Stop(name string) (*runner.Status, error)
+	Status(name string) (*runner.Status, error)
+	Output(name string) (output <-chan []byte, cancel func(), err error)
+}
+
+// Start a job and return the result which contains the job ID.
+func (p *piled) Start(_ context.Context, req *pb.JobStartRequest) (*pb.JobStartResult, error) {
+	logrus.Tracef("start %+v", req)
+	if err := p.opAuthorized(req.Token, "start"); err != nil {
+		return nil, err
+	}
+	// TODO verify whether command is well formed
+	config := runner.Config{
+		Command: req.Command,
+	}
+	if req.Resources != nil {
+		config.CPUMax = req.Resources.CPUMax
+		config.MemoryMax = req.Resources.MemoryMax
+		config.IOMax = req.Resources.IOMax
+	}
+	jobName, err := p.runner.Start(config)
+	if err != nil {
+		return nil, fmt.Errorf("cannot start job: %v", err)
+	}
+	return &pb.JobStartResult{
+		ID: jobName,
+	}, nil
+}
+
+func statusFromJobStatus(status *runner.Status) *pb.Status {
+	s := &pb.Status{}
+	switch {
+	case !status.Active && status.Signal == int(syscall.SIGKILL) && status.OOM:
+		s.Status = pb.Status_OOM_KILLED
+	case !status.Active && status.ExitStatus != 0:
+		s.Status = pb.Status_FAILED
+	case !status.Active:
+		s.Status = pb.Status_EXITED
+	case status.Active:
+		s.Status = pb.Status_ACTIVE
+	}
+	if !status.Active {
+		s.ExitStatus = int32(status.ExitStatus)
+		s.TermSignal = int32(status.Signal)
+	}
+	return s
+}
+
+// Stop a given job.
+func (p *piled) Stop(_ context.Context, req *pb.JobRequest) (*pb.StopResult, error) {
+	logrus.Tracef("stop %+v", req)
+	if err := p.opAuthorized(req.Token, "stop"); err != nil {
+		return nil, err
+	}
+	status, err := p.runner.Stop(req.ID)
+	if err == runner.JobNotFoundError {
+		return nil, fmt.Errorf("cannot stop nonexistent job %v", req.ID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("cannot stop job: %v", err)
+	}
+	// TODO clean job logs
+	return &pb.StopResult{
+		Status: statusFromJobStatus(status),
+	}, nil
+}
+
+// Status obtains the status of a given job.
+func (p *piled) Status(_ context.Context, req *pb.JobRequest) (*pb.StatusResult, error) {
+	logrus.Tracef("status %+v", req)
+	if err := p.opAuthorized(req.Token, "status"); err != nil {
+		return nil, err
+	}
+	status, err := p.runner.Status(req.ID)
+	if err != nil {
+		if err == runner.JobNotFoundError {
+			return nil, fmt.Errorf("cannot obtain a status of nonexistent job")
+		}
+		return nil, fmt.Errorf("cannot obtain job status: %v", err)
+	}
+	return &pb.StatusResult{
+		Status: statusFromJobStatus(status),
+	}, nil
+}
+
+// Output obtains the output of a given job.
+func (p *piled) Output(req *pb.JobRequest, out pb.JobPileManager_OutputServer) error {
+	logrus.Tracef("output %+v", req)
+	if err := p.opAuthorized(req.Token, "output"); err != nil {
+		return err
+	}
+	output, cancel, err := p.runner.Output(req.ID)
+	if err != nil {
+		return fmt.Errorf("cannot start collecting job output: %v", err)
+	}
+
+	for {
+		logrus.Tracef("waiting for logs")
+		select {
+		case chunk, ok := <-output:
+			logrus.Tracef("got %vB of output", len(chunk))
+			if !ok {
+				logrus.Tracef("logs closed")
+				return nil
+			}
+			if err := out.Send(&pb.OutputChunk{Chunk: chunk}); err != nil {
+				logrus.Tracef("send failed: %v", err)
+				cancel()
+				return err
+			}
+		case <-out.Context().Done():
+			logrus.Tracef("output context done")
+			cancel()
+			return out.Context().Err()
+		}
+	}
+}
+
+var NotAuthorizedError = fmt.Errorf("not authorized")
+
+func (p *piled) opAuthorized(tok, op string) error {
+	opsForToken, ok := p.opAuthz[tok]
+	if !ok {
+		// unknown token
+		return NotAuthorizedError
+	}
+	i := sort.SearchStrings(opsForToken, op)
+	if i < len(opsForToken) && opsForToken[i] == op {
+		// op is allowed for token
+		return nil
+	}
+	return NotAuthorizedError
+}
+
+// Serve makes the server implementation available on a given listener
+func (p *PileServer) Serve(ctx context.Context, l net.Listener, authConfig auth.Config) error {
+	creds := credentials.NewTLS(auth.ServerTLSConfig(authConfig))
+	gsrv := grpc.NewServer(grpc.Creds(creds))
+	pb.RegisterJobPileManagerServer(gsrv, &p.pile)
+
+	if ctx != nil {
+		go func() {
+			<-ctx.Done()
+			gsrv.GracefulStop()
+		}()
+	}
+	return gsrv.Serve(l)
+}
+
+type PileServer struct {
+	pile piled
+}
+
+func NewWithRunner(runner Runner) *PileServer {
+	allOps := []string{"start", "stop", "status", "output"}
+	roOps := []string{"status", "output"}
+	sort.Strings(allOps)
+	sort.Strings(roOps)
+
+	return &PileServer{
+		pile: piled{
+			runner: runner,
+
+			opAuthz: map[string][]string{
+				"admin-token": allOps,
+				"ro-token":    roOps,
+			},
+		},
+	}
+}
---
+++ pile/server/server_test.go
@@ -0,0 +1,433 @@
+package server_test
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"net"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/test/bufconn"
+
+	"github.com/bboozzoo/piled/pile/auth"
+	"github.com/bboozzoo/piled/pile/proto"
+	pb "github.com/bboozzoo/piled/pile/proto"
+	"github.com/bboozzoo/piled/pile/server"
+	"github.com/bboozzoo/piled/runner"
+	"github.com/bboozzoo/piled/testutils"
+)
+
+type mockRunner struct {
+	start  func(config runner.Config) (name string, err error)
+	stop   func(name string) (*runner.Status, error)
+	status func(name string) (*runner.Status, error)
+	output func(name string) (<-chan []byte, func(), error)
+}
+
+var errNotImplemnted = errors.New("mock not implemented")
+
+func (m *mockRunner) Start(config runner.Config) (name string, err error) {
+	if m.start != nil {
+		return m.start(config)
+	}
+	return "", errNotImplemnted
+}
+
+func (m *mockRunner) Stop(name string) (*runner.Status, error) {
+	if m.stop != nil {
+		return m.stop(name)
+	}
+	return nil, errNotImplemnted
+}
+func (m *mockRunner) Status(name string) (*runner.Status, error) {
+	if m.status != nil {
+		return m.status(name)
+	}
+	return nil, errNotImplemnted
+}
+
+func (m *mockRunner) Output(name string) (output <-chan []byte, cancel func(), err error) {
+	if m.output != nil {
+		return m.output(name)
+	}
+	return nil, nil, errNotImplemnted
+}
+
+func TestSimpleTokenAuthzStart(t *testing.T) {
+	startCalled := 0
+	jobName := "pile." + uuid.NewString()
+	s := server.NewWithRunner(&mockRunner{
+		start: func(_ runner.Config) (string, error) {
+			startCalled++
+			return jobName, nil
+		},
+	}).Server()
+	res, err := s.Start(context.Background(), &proto.JobStartRequest{
+		Token: "bad",
+	})
+	require.EqualError(t, err, server.NotAuthorizedError.Error())
+	require.Nil(t, res)
+	assert.Equal(t, 0, startCalled)
+
+	res, err = s.Start(context.Background(), &proto.JobStartRequest{
+		Token: "ro-token",
+	})
+	require.EqualError(t, err, server.NotAuthorizedError.Error())
+	require.Nil(t, res)
+	assert.Equal(t, 0, startCalled)
+
+	res, err = s.Start(context.Background(), &proto.JobStartRequest{
+		Token: "admin-token",
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	assert.Equal(t, 1, startCalled)
+	assert.Regexp(t, `pile\.[a-z0-9]+(-[a-z0-9]+)+`, res.ID)
+	assert.Equal(t, jobName, res.ID)
+}
+
+func TestSimpleJobCycle(t *testing.T) {
+	startCalled := 0
+	jobName := "job." + uuid.NewString()
+	s := server.NewWithRunner(&mockRunner{
+		start: func(_ runner.Config) (string, error) {
+			startCalled++
+			return jobName, nil
+		},
+		stop: func(name string) (*runner.Status, error) {
+			require.Equal(t, jobName, name)
+			return &runner.Status{
+				Active:     false,
+				Signal:     int(syscall.SIGKILL),
+				OOM:        true,
+				ExitStatus: -1,
+			}, nil
+		},
+		status: func(name string) (*runner.Status, error) {
+			require.Equal(t, jobName, name)
+			return &runner.Status{
+				Active: true,
+			}, nil
+		},
+	}).Server()
+	res, err := s.Start(context.Background(), &proto.JobStartRequest{
+		Token: "admin-token",
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	assert.Equal(t, 1, startCalled)
+	assert.Equal(t, jobName, res.ID)
+
+	statusRes, err := s.Status(context.Background(), &proto.JobRequest{
+		Token: "ro-token",
+		ID:    res.ID,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, statusRes)
+	assert.Equal(t, &pb.StatusResult{
+		Status: &pb.Status{
+			Status: pb.Status_ACTIVE,
+		},
+	}, statusRes)
+
+	stopRes, err := s.Stop(context.Background(), &proto.JobRequest{
+		Token: "admin-token",
+		ID:    res.ID,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, stopRes)
+	assert.Equal(t, &pb.StopResult{
+		Status: &pb.Status{
+			Status:     pb.Status_OOM_KILLED,
+			ExitStatus: -1,
+			TermSignal: int32(syscall.SIGKILL),
+		},
+	}, stopRes)
+
+}
+
+type mockOutputServer struct {
+	grpc.ServerStream
+	ctx  context.Context
+	send func(*pb.OutputChunk) error
+}
+
+func (m *mockOutputServer) Send(chunk *pb.OutputChunk) error {
+	return m.send(chunk)
+}
+
+func (m *mockOutputServer) Context() context.Context {
+	return m.ctx
+}
+
+type testOutputCase struct {
+	callCancel bool
+}
+
+func testOutput(t *testing.T, tc testOutputCase) {
+	startCalled := 0
+	// setup a mock output channel, which 3 chunks
+	outputChan := make(chan []byte, 3)
+	outputChan <- []byte("hello ")
+	outputChan <- []byte("from ")
+	outputChan <- []byte("test")
+	runnerCancelCalls := 0
+	// mock cancel which runner.Output() returns
+	runnerCancel := func() {
+		if !assert.Equal(t, 0, runnerCancelCalls) {
+			panic("foo")
+		}
+		runnerCancelCalls++
+		close(outputChan)
+	}
+	s := server.NewWithRunner(&mockRunner{
+		start: func(_ runner.Config) (string, error) {
+			startCalled++
+			// use something more realistic
+			return "pile." + uuid.NewString(), nil
+		},
+		output: func(name string) (<-chan []byte, func(), error) {
+			return outputChan, runnerCancel, nil
+		},
+	}).Server()
+	res, err := s.Start(context.Background(), &proto.JobStartRequest{
+		Token: "admin-token",
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	assert.Equal(t, 1, startCalled)
+	assert.Regexp(t, `pile\.[a-z0-9]+(-[a-z0-9]+)+`, res.ID)
+
+	var buf bytes.Buffer
+	// pretend we're streaming the output, but close channel after sending
+	// the last chunk
+	outputCtx, outputCancel := context.WithCancel(context.Background())
+	sendCalls := 0
+	err = s.Output(&pb.JobRequest{
+		Token: "ro-token",
+		ID:    res.ID,
+	}, &mockOutputServer{
+		ctx: outputCtx,
+		send: func(chunk *pb.OutputChunk) error {
+			sendCalls++
+			buf.Write(chunk.Chunk)
+			if sendCalls == 3 {
+				if tc.callCancel {
+					// pretend that the called has canceled the call
+					// due for eg. connection interrupt
+					outputCancel()
+				} else {
+					// pretend output is complete and the
+					// job has finished
+					close(outputChan)
+				}
+			}
+			return nil
+		},
+	})
+	if tc.callCancel {
+		require.Error(t, err, context.Canceled)
+		// the server's output called the runner's cancel when its context was
+		// canceled
+		assert.Equal(t, 1, runnerCancelCalls)
+	} else {
+		require.NoError(t, err)
+		assert.Equal(t, 0, runnerCancelCalls)
+	}
+	assert.Equal(t, "hello from test", buf.String())
+	assert.Equal(t, 3, sendCalls)
+}
+
+func TestOutput(t *testing.T) {
+	t.Run("with_cancel", func(t *testing.T) {
+		testOutput(t, testOutputCase{
+			callCancel: true,
+		})
+	})
+	t.Run("collect_all", func(t *testing.T) {
+		testOutput(t, testOutputCase{
+			callCancel: false,
+		})
+	})
+}
+
+type testDataSet struct {
+	CAFile                string
+	ClientCert, ClientKey string
+	ServerCert, ServerKey string
+}
+
+func testData(t *testing.T, tds testDataSet) (CAPool *x509.CertPool, server, client tls.Certificate) {
+	client, err := tls.LoadX509KeyPair(tds.ClientCert, tds.ClientKey)
+	require.NoError(t, err)
+	server, err = tls.LoadX509KeyPair(tds.ServerCert, tds.ServerKey)
+	require.NoError(t, err)
+	pool := x509.NewCertPool()
+	d, err := os.ReadFile(tds.CAFile)
+	require.NoError(t, err)
+	pool.AppendCertsFromPEM(d)
+	return pool, server, client
+}
+
+type serverCertTestCase struct {
+	tds   testDataSet
+	valid bool
+	log   string
+}
+
+func testServeValidCerts(t *testing.T, tc serverCertTestCase) {
+	logBuf := testutils.MockLogger(t)
+	pool, serverCert, clientCert := testData(t, tc.tds)
+	s := server.NewWithRunner(&mockRunner{
+		start: func(config runner.Config) (name string, err error) {
+			return "1234", nil
+		},
+	})
+	l := bufconn.Listen(4096)
+	ac := auth.Config{
+		CAPool: pool,
+		Cert:   serverCert,
+	}
+	closeC := make(chan struct{})
+	serveCtx, serveCancel := context.WithCancel(context.Background())
+	go func() {
+		err := s.Serve(serveCtx, l, ac)
+		require.NoError(t, err)
+		t.Logf("serve done")
+		close(closeC)
+	}()
+	dial := func(context.Context, string) (net.Conn, error) {
+		return l.Dial()
+	}
+	var gotServerCert *x509.Certificate
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		MinVersion:   tls.VersionTLS13,
+		// do our own verification instead
+		InsecureSkipVerify: true,
+		VerifyConnection: func(cs tls.ConnectionState) error {
+			assert.Len(t, cs.PeerCertificates, 1)
+			gotServerCert = cs.PeerCertificates[0]
+			return nil
+		},
+	})
+
+	conn, err := grpc.Dial("",
+		grpc.WithContextDialer(dial),
+		grpc.WithTransportCredentials(creds))
+	require.NoError(t, err)
+	c := pb.NewJobPileManagerClient(conn)
+	res, err := c.Start(context.Background(), &pb.JobStartRequest{
+		Token:   "admin-token",
+		Command: []string{"ls", "-l"},
+	})
+	if tc.valid {
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		assert.Equal(t, "1234", res.ID)
+	} else {
+		t.Logf("err: %v", err)
+		require.Error(t, err)
+		assert.Regexp(t, ".*connection closed before server preface received", err.Error())
+	}
+	// verify that the server certificate is the same as passed in the
+	// config
+	require.NotNil(t, gotServerCert)
+	wantServerCert, err := x509.ParseCertificate(serverCert.Certificate[0])
+	require.NoError(t, err)
+	assert.True(t, wantServerCert.Equal(gotServerCert))
+
+	conn.Close()
+	serveCancel()
+	<-closeC
+	l.Close()
+
+	if tc.log != "" {
+		assert.Contains(t, logBuf.String(), tc.log)
+	} else {
+		assert.Empty(t, logBuf.String())
+	}
+}
+
+func TestServerCertValidation(t *testing.T) {
+	t.Run("valid_cert", func(t *testing.T) {
+		testServeValidCerts(t, serverCertTestCase{
+			tds: testDataSet{
+				CAFile:     "../../testdata/ca-cert.pem",
+				ClientCert: "../../testdata/client-cert.pem",
+				ClientKey:  "../../testdata/client-key.pem",
+				ServerCert: "../../testdata/server-cert.pem",
+				ServerKey:  "../../testdata/server-key.pem",
+			},
+			valid: true,
+		})
+	})
+	t.Run("invalid_cert", func(t *testing.T) {
+		testServeValidCerts(t, serverCertTestCase{
+			tds: testDataSet{
+				CAFile:     "../../testdata/ca-cert.pem",
+				ClientCert: "../../testdata/invalid-client-cert.pem",
+				ClientKey:  "../../testdata/invalid-client-key.pem",
+				ServerCert: "../../testdata/server-cert.pem",
+				ServerKey:  "../../testdata/server-key.pem",
+			},
+			valid: false,
+		})
+	})
+	t.Run("valid_cert_not_pilec", func(t *testing.T) {
+		testServeValidCerts(t, serverCertTestCase{
+			tds: testDataSet{
+				CAFile:     "../../testdata/ca-cert.pem",
+				ClientCert: "../../testdata/client-not-pilec-cert.pem",
+				ClientKey:  "../../testdata/client-key.pem",
+				ServerCert: "../../testdata/server-cert.pem",
+				ServerKey:  "../../testdata/server-key.pem",
+			},
+			valid: false,
+			// quotes in a message are escaped
+			log: `expected certificate of pilec not \"CN=not-pilec,O=pile\"`,
+		})
+	})
+}
+
+func TestAuthorization(t *testing.T) {
+	type opsAuthz struct {
+		ok    []string
+		notOk []string
+	}
+	tokensAndOps := map[string]opsAuthz{
+		"admin-token": {
+			ok: []string{"start", "stop", "status", "output"},
+		},
+		"ro-token": {
+			ok:    []string{"status", "output"},
+			notOk: []string{"start", "stop"},
+		},
+		"unknown-token": {
+			notOk: []string{"start", "stop", "status", "output"},
+		},
+	}
+
+	s := server.NewWithRunner(&mockRunner{})
+
+	for tok, ops := range tokensAndOps {
+		for _, op := range ops.ok {
+			err := s.OpAuthorized(tok, op)
+			assert.NoError(t, err, "tok %v op: %v", tok, op)
+		}
+		for _, op := range ops.notOk {
+			err := s.OpAuthorized(tok, op)
+			assert.EqualError(t, err, server.NotAuthorizedError.Error(),
+				"tok: %v op: %v", tok, op)
+		}
+	}
+}
