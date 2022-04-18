2f80a8dd024727b0f9a61b9a480c704921b8b29e
Maciek Borzecki <maciek.borzecki@gmail.com>
2022-04-18 22:34:01 +0200

    testutils: always restore os.Stderr
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- testutils/logs.go
+++ testutils/logs.go
@@ -9,12 +9,11 @@ import (
 )
 
 func MockLogger(t *testing.T) *bytes.Buffer {
-	oldOut := os.Stderr
 	buf := &bytes.Buffer{}
 	logrus.SetOutput(buf)
 
 	t.Cleanup(func() {
-		logrus.SetOutput(oldOut)
+		logrus.SetOutput(os.Stderr)
 	})
 	return buf
 }
